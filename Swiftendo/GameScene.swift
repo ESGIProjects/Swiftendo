f2466ce73ddae9704ec82bc9c6c47fabb25be0ce
Jason Pierna <siilver777@me.com>
2017-12-05 09:09:28 +0100

    Code cleanup

--- Swiftendo/GameScene.swift
+++ Swiftendo/GameScene.swift
@@ -18,16 +18,17 @@ class GameScene: SKScene {
 	
     //init parameters for music
     var backgroundMusic: AVAudioPlayer?
-    var musicNumber: Int = 0
-    var path : String!
-    
+    var musicNumber = 0
+	
     //buttons
-    let upButton = SKSpriteNode(imageNamed: "Button_up")
-    let downButton = SKSpriteNode(imageNamed: "Button_down")
-    let leftButton = SKSpriteNode(imageNamed: "Button_left")
-    let rightButton = SKSpriteNode(imageNamed: "Button_right")
-    let startButton = SKSpriteNode(imageNamed: "Start")
-    let actionButton = SKSpriteNode(imageNamed: "A")
+	var upButton: SKSpriteNode!
+    var downButton: SKSpriteNode!
+    var leftButton: SKSpriteNode!
+    var rightButton: SKSpriteNode!
+    var startButton: SKSpriteNode!
+    var actionButton: SKSpriteNode!
+	
+	let player = SKShapeNode(rectOf: CGSize(width: 16, height: 16))
     
     let musicList = [
 		"Sounds/Hyrule_Castle_SNES",
@@ -38,7 +39,6 @@ class GameScene: SKScene {
 		"Sounds/Gerudo_Valley_N64",
 		"Sounds/Tal_Tal_Mountain_GB"
 	]
-	
     
     override func didMove(to view: SKView) {
 		cameraNode = camera!
@@ -46,82 +46,25 @@ class GameScene: SKScene {
         playBackgroundMusic()
         initButtons()
     }
-    
-    func playBackgroundMusic() {
-		
-		let selectedMusic = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: musicList).first as! String
-		
-		if let path = Bundle.main.path(forResource: selectedMusic, ofType: "mp3") {
-			let url = URL(fileURLWithPath: path)
-			backgroundMusic = try? AVAudioPlayer(contentsOf: url)
-			backgroundMusic?.play()
-			backgroundMusic?.delegate = self
-		}
-    }
-    
-    func initButtons(){
-        
-        let sizes = self.frame
-        
-        upButton.anchorPoint = CGPoint(x:0,y:0)
-        upButton.position = CGPoint(x:sizes.minX+75,y:sizes.minY+85)
-        upButton.xScale = 0.15
-        upButton.yScale = 0.15
-		upButton.name = "button"
-        cameraNode.addChild(upButton)
-        
-        downButton.anchorPoint = CGPoint(x:0,y:0)
-        downButton.position = CGPoint(x:sizes.minX+75,y:sizes.minY+25)
-        downButton.xScale = 0.15
-        downButton.yScale = 0.15
-		downButton.name = "button"
-        cameraNode.addChild(downButton)
-        
-        leftButton.anchorPoint = CGPoint(x:0,y:0)
-        leftButton.position = CGPoint(x:sizes.minX+40,y:sizes.minY+57.5)
-        leftButton.xScale = 0.15
-        leftButton.yScale = 0.15
-		leftButton.name = "button"
-        cameraNode.addChild(leftButton)
-        
-        rightButton.anchorPoint = CGPoint(x:0,y:0)
-        rightButton.position = CGPoint(x:sizes.minX+105,y:sizes.minY+57.5)
-        rightButton.xScale = 0.15
-        rightButton.yScale = 0.15
-		rightButton.name = "button"
-        cameraNode.addChild(rightButton)
-        
-        startButton.position = CGPoint(x:0,y:sizes.minY+30)
-        startButton.xScale = 0.6
-        startButton.yScale = 0.6
-		startButton.name = "button"
-        cameraNode.addChild(startButton)
-        
-        actionButton.position = CGPoint(x:sizes.maxX-70,y:sizes.minY+72.5)
-        actionButton.xScale = 0.6
-        actionButton.yScale = 0.6
-		actionButton.name = "button"
-        cameraNode.addChild(actionButton)
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+	
+	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
 		guard let touch = touches.first else { return }
-		lastTouch = touch.location(in: self.view)
+		lastTouch = touch.location(in: view)
 		originalTouch = lastTouch
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+	}
+	
+	override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
 		guard let touch = touches.first else { return }
-		let touchLocation = touch.location(in: self.view)
+		let touchLocation = touch.location(in: view)
 		
 		let newX = cameraNode.position.x + (lastTouch.x - touchLocation.x)
 		let newY = cameraNode.position.y + (touchLocation.y - lastTouch.y)
 		
 		cameraNode.position = CGPoint(x: newX, y: newY)
 		lastTouch = touchLocation
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+	}
+	
+	override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
 		guard let touch = touches.first else { return }
 		
 		let tappedNodes = nodes(at: touch.location(in: cameraNode))
@@ -133,10 +76,69 @@ class GameScene: SKScene {
 				print("Random touch")
 			}
 		}
+	}
+	
+	override func update(_ currentTime: TimeInterval) {
+		// Called before each frame is rendered
+	}
+    
+    func playBackgroundMusic() {
+		let selectedMusic = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: musicList).first as! String
+		
+		if let path = Bundle.main.path(forResource: selectedMusic, ofType: "mp3") {
+			let url = URL(fileURLWithPath: path)
+			backgroundMusic = try? AVAudioPlayer(contentsOf: url)
+			//backgroundMusic?.play()
+			backgroundMusic?.delegate = self
+		}
     }
-
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+    
+    func initButtons() {
+		upButton = SKSpriteNode(imageNamed: "Button_up")
+		upButton.anchorPoint = CGPoint(x:0,y:0)
+        upButton.position = CGPoint(x: frame.minX + 75, y: frame.minY + 85)
+        upButton.xScale = 0.15
+        upButton.yScale = 0.15
+		upButton.name = "button"
+        cameraNode.addChild(upButton)
+		
+		downButton = SKSpriteNode(imageNamed: "Button_down")
+        downButton.anchorPoint = CGPoint(x:0,y:0)
+        downButton.position = CGPoint(x: frame.minX + 75, y: frame.minY + 25)
+        downButton.xScale = 0.15
+        downButton.yScale = 0.15
+		downButton.name = "button"
+        cameraNode.addChild(downButton)
+		
+		leftButton = SKSpriteNode(imageNamed: "Button_left")
+        leftButton.anchorPoint = CGPoint(x:0,y:0)
+        leftButton.position = CGPoint(x: frame.minX + 40, y: frame.minY + 57.5)
+        leftButton.xScale = 0.15
+        leftButton.yScale = 0.15
+		leftButton.name = "button"
+        cameraNode.addChild(leftButton)
+		
+		rightButton = SKSpriteNode(imageNamed: "Button_right")
+        rightButton.anchorPoint = CGPoint(x:0,y:0)
+        rightButton.position = CGPoint(x: frame.minX + 105, y: frame.minY + 57.5)
+        rightButton.xScale = 0.15
+        rightButton.yScale = 0.15
+		rightButton.name = "button"
+        cameraNode.addChild(rightButton)
+		
+		startButton = SKSpriteNode(imageNamed: "Start")
+        startButton.position = CGPoint(x: 0,y: frame.minY + 30)
+        startButton.xScale = 0.6
+        startButton.yScale = 0.6
+		startButton.name = "button"
+        cameraNode.addChild(startButton)
+		
+		actionButton = SKSpriteNode(imageNamed: "A")
+        actionButton.position = CGPoint(x: frame.maxX - 70, y: frame.minY + 72.5)
+        actionButton.xScale = 0.6
+        actionButton.yScale = 0.6
+		actionButton.name = "button"
+        cameraNode.addChild(actionButton)
     }
 }
 
