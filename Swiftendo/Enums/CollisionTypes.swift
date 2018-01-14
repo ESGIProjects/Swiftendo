abee285911d0c63eddcc0862156e96f96d70c853
Jason Pierna <siilver777@me.com>
2018-01-14 22:59:29 +0100

    New CollisionTypes enum

---
+++ Swiftendo/Enums/CollisionTypes.swift
@@ -0,0 +1,15 @@
+//
+//  CollisionTypes.swift
+//  Swiftendo
+//
+//  Created by Jason Pierna on 14/01/2018.
+//  Copyright © 2018 ESGI. All rights reserved.
+//
+
+import Foundation
+
+enum CollisionTypes: UInt32 {
+	case player		= 1
+	case monster	= 2
+	case pokeball	= 4
+}
