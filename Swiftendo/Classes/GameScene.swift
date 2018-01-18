cefc924f7587d381220bc584e279fd30ec61d91a
Jason Pierna <siilver777@me.com>
2018-01-18 14:14:16 +0100

    Buttons updated, gitignore updated, UITouch specific variables deleted. Game Over started

--- Swiftendo/Classes/GameScene.swift
+++ Swiftendo/Classes/GameScene.swift
@@ -19,11 +19,6 @@ class GameScene: SKScene {
 	var cameraNode: SKCameraNode!
 	var tileMapNode: SKTileMapNode!
 	
-	// MARK: - Touch properties
-	
-	var lastTouch: CGPoint = .zero
-	var originalTouch: CGPoint = .zero
-	
 	// MARK: - Music properties
 	
     var backgroundMusic: AVAudioPlayer?
@@ -41,12 +36,14 @@ class GameScene: SKScene {
 	
 	// MARK: - Button properties
 	
+	var buttons = [String: Button]()
+	/*
 	var upButton: Button!
     var downButton: Button!
     var leftButton: Button!
     var rightButton: Button!
     var startButton: Button!
-    var actionButton: Button!
+    var actionButton: Button!*/
 	
 	// MARK: - Entity properties
 	
@@ -72,16 +69,14 @@ class GameScene: SKScene {
 		addChild(player.node)
 		
 		// Initialize music & buttons
-        //playBackgroundMusic()
+        playBackgroundMusic()
         initButtons()
 		
 		// Constraints the camera
 		setCameraConstraints()
 		
 		// Start enemy spawn 5 seconds later
-		Timer.scheduledTimer(withTimeInterval: 5, repeats: false) { [unowned self] _ in
-			self.launchMonsterGeneration(timeInterval: 10)
-		}
+		self.launchMonsterGeneration(timeInterval: 5)
     }
 	
 	override func update(_ currentTime: TimeInterval) {
@@ -110,63 +105,69 @@ class GameScene: SKScene {
 		if let path = Bundle.main.path(forResource: selectedMusic, ofType: "mp3") {
 			let url = URL(fileURLWithPath: path)
 			backgroundMusic = try? AVAudioPlayer(contentsOf: url)
-			backgroundMusic?.play()
 			backgroundMusic?.delegate = self
+			backgroundMusic?.play()
 		}
     }
     
     func initButtons() {
-		upButton = Button(type: .up)
+		let upButton = Button(type: .up)
 		upButton.action = {[unowned self] in self.touchButton(.up) }
 		upButton.anchorPoint = CGPoint(x: 0,y: 0)
         upButton.position = CGPoint(x: frame.minX + 75, y: frame.minY + 85)
         upButton.xScale = 0.15
         upButton.yScale = 0.15
 		upButton.alpha = 0.5
-        cameraNode.addChild(upButton)
+		buttons["up"] = upButton
+		cameraNode.addChild(upButton)
 		
-		downButton = Button(type: .down)
+		let downButton = Button(type: .down)
 		downButton.action = {[unowned self] in self.touchButton(.down) }
         downButton.anchorPoint = CGPoint(x: 0,y: 0)
         downButton.position = CGPoint(x: frame.minX + 75, y: frame.minY + 25)
         downButton.xScale = 0.15
         downButton.yScale = 0.15
 		downButton.alpha = 0.5
+		buttons["down"] = downButton
         cameraNode.addChild(downButton)
 		
-		leftButton = Button(type: .left)
+		let leftButton = Button(type: .left)
 		leftButton.action = {[unowned self] in self.touchButton(.left) }
         leftButton.anchorPoint = CGPoint(x: 0,y: 0)
         leftButton.position = CGPoint(x: frame.minX + 40, y: frame.minY + 57.5)
         leftButton.xScale = 0.15
         leftButton.yScale = 0.15
 		leftButton.alpha = 0.5
+		buttons["left"] = leftButton
         cameraNode.addChild(leftButton)
 		
-		rightButton = Button(type: .right)
+		let rightButton = Button(type: .right)
 		rightButton.action = {[unowned self] in self.touchButton(.right) }
         rightButton.anchorPoint = CGPoint(x: 0,y: 0)
         rightButton.position = CGPoint(x: frame.minX + 105, y: frame.minY + 57.5)
         rightButton.xScale = 0.15
         rightButton.yScale = 0.15
 		rightButton.alpha = 0.5
+		buttons["right"] = rightButton
         cameraNode.addChild(rightButton)
 		
-		startButton = Button(type: .start)
+		let startButton = Button(type: .start)
 		startButton.action = {[unowned self] in self.touchButton(.start) }
         startButton.position = CGPoint(x: 0,y: frame.minY + 30)
         startButton.xScale = 0.6
         startButton.yScale = 0.6
 		startButton.alpha = 0.5
+		buttons["start"] = startButton
         cameraNode.addChild(startButton)
 
         if !(session?.isReachable)! || !(session?.isPaired)!{
-            actionButton = Button(type: .action)
+            let actionButton = Button(type: .action)
             actionButton.action = {[unowned self] in self.touchButton(.action) }
             actionButton.position = CGPoint(x: frame.maxX - 70, y: frame.minY + 72.5)
             actionButton.xScale = 0.6
             actionButton.yScale = 0.6
             actionButton.alpha = 0.5
+			buttons["action"] = actionButton
             cameraNode.addChild(actionButton)
         }
     }
@@ -267,8 +268,29 @@ class GameScene: SKScene {
 		return nil
 	}
 	
+	// MARK: - Pause & End game
+	
 	func gameOver() {
 		print("Game over!")
+		
+		/*
+		for monster in monsters {
+			monster.node.removeAction(forKey: "follow")
+		}
+		*/
+		
+		// Overlay
+		let overlay = SKShapeNode(rectOf: self.size)
+		overlay.fillColor = UIColor.black.withAlphaComponent(0.5)
+		overlay.position = CGPoint(x: 0, y: 0)
+		overlay.zPosition = 1 // on top of buttons
+		
+		// Add game over on overlay
+		
+
+		// Add overlay
+		cameraNode.addChild(overlay)
+
 	}
 	
 	func restart() {
